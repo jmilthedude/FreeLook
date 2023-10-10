1da895832c6a380ff1921ff9d8430550add56429
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2023-10-09 19:56:11 -0500

    fix imports

--- common/src/main/java/net/ninjadev/freelook/mixin/GameRendererMixin.java
+++ common/src/main/java/net/ninjadev/freelook/mixin/GameRendererMixin.java
@@ -1,10 +1,8 @@
 package net.ninjadev.freelook.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Matrix4f;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
-import net.ninjadev.freelook.event.CameraEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
